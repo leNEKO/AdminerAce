099c0b4c8b544c977868c49b6ba654abc7ea301f
leNEKO <le.neko@gmail.com>
2018-09-26 16:45:50 +0200

    First commit

---
+++ AdminerMonaco.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Use monaco editor for the sql request editor
+ *
+ * @link https://…
+ *
+ * @author Nicolas Masson
+ * @copyright 2014-2018 Pematon, s.r.o. (http://leneko.github.com/)
+ */
+
+class AdminerMonaco
+{
+
+    public function head()
+    {
+        die("hello");
+    }
+}
